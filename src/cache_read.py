5a006ea7147498f37d4ed95690040ce304a40508
Richard Pappalardo <rpappalax@gmail.com>
2023-07-27 23:59:27 +0200

    actions

--- src/cache_read.py
+++ src/cache_read.py
@@ -12,17 +12,25 @@ def version_last_read():
 #    with open(FILENAME, 'w') as file:
 #        file.write('100.2')
 
+def current_time():
+    current_time = datetime.datetime.now()
+    return current_time.strftime('%H:%M:%S')
+
 
 def version_last_write():
-    current_time = datetime.datetime.now()
-    formatted_time = current_time.strftime('%H:%M:%S')
     with open(FILENAME, "w") as file:
         file.write(formatted_time)
 
+def is_different(vers_a, vers_b):
+    return (vers_a == vers_b)
 
 if __name__ == '__main__':
     
-    version = version_last_read()
-    print(f'LAST VERSION: {version}')
+    version_a = version_last_read()
+    print(f'LAST VERSION (from file): {version_a}')
+    version_b = current_time()
+    print(f'CURRENT TIME: {version_b}')
+    diff = is_different(version_a, version_b)
+    print(f'IS_DIFFERENT: {diff}')
     
 
