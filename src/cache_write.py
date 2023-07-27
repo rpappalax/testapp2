6e93ace05b671735320a65c12b610bd985290b16
Richard Pappalardo <rpappalax@gmail.com>
2023-07-27 23:43:19 +0200

    actions

---
+++ src/cache_write.py
@@ -0,0 +1,29 @@
+import os
+import datetime
+
+
+FILENAME = 'src/version_last.txt'
+
+def version_last_read():
+    with open(FILENAME, "r") as file:
+        return file.read()
+
+#def version_last_write():
+#    with open(FILENAME, 'w') as file:
+#        file.write('100.2')
+
+
+def version_last_write():
+    current_time = datetime.datetime.now()
+    formatted_time = current_time.strftime('%H:%M:%S')
+    with open(FILENAME, "w") as file:
+        file.write(formatted_time)
+
+
+if __name__ == '__main__':
+    
+    version_last_write()
+    version = version_last_read()
+    print(f'LAST VERSION (after write): {version}')
+    
+
